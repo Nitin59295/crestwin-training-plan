806db34e8f13dc87c3512c020efc3cd8c590f656
Nitin Bhatia <bhatianitin49@gmail.com>
2026-07-21 18:08:48 +0530

    Completed Day03

---
+++ Day03/Fstring.py
@@ -0,0 +1,21 @@
+# Fstrings : it is the morden way to format strings by inserting elements inside them
+
+
+name = "Nitin"
+age = 25
+print(f"Hi my name is {name}, i am {age} yr's old")
+print(f"Name :- {name}, age :- {age+1}")
+
+
+
+
+#Slicing : it is a way of extracting a part of string using its index positions
+
+x = [1,2,3,4,5,6]
+print(x[0:3])  #-- prints [1,2,3]
+
+
+s = "python"
+print(s[0:4])  #-- prints [pyth]
+
+
